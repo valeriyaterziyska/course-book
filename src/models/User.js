620950db2d7a27ccd0ec307f20a59ab18c6600f6
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-26 12:53:51 +0200

    Add validation and hashing

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -15,6 +16,17 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, 12);
+});
+
+userSchema.virtual('rePassword')
+    .set(function(value) {
+        if(value !== this.password) {
+            throw new Error('Password missmatch');
+        }
+    });
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
