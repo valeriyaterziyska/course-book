e4ab5da39996bc01935cd64ef0dc28760333b1a4
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-26 11:13:55 +0200

    Add middleware body-parser

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static('src/public'));
+app.use(express.static("src/public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.send("Hello world");
