c3ef7b687d58b39bdf264c487f6a7ceeafc47405
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-26 15:18:44 +0200

    Delete jwt ts definition

--- src/lib/jsonwebtoken.d.ts
+++
@@ -1,8 +0,0 @@
-const sign = (payload: string | object | Buffer, secret: string) =>
-    Promise<string>;
-const verify = (token: string, secret: string) => Promise<object>;
-
-export = {
-    sign,
-    verify,
-};
