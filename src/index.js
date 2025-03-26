02ccf0d79f5451d0e7bce6c81d0cf1268570af4c
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-26 11:17:19 +0200

    Add module route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
+const routes = require('./routes');
+
 const app = express();
 
 app.use(express.static("src/public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(routes);
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-});
 
 app.listen(5000, () => console.log(`App is listening on port 5000...`));
