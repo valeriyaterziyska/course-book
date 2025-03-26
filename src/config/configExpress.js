3b2dee3addd0b8f92b232e4877bb7417e421c405
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-26 14:00:10 +0200

    Add authMiddleware to configExpress

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,11 +1,13 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const path = require('path');
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
 function configExpress(app) {
     app.use(express.static(path.resolve('src/public')));
     app.use(express.urlencoded({extended: false}));
     app.use(cookieParser());
+    app.use(authMiddleware);
 
     return app;
 }
