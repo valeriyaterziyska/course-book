45b1a63b210302c1cde1cd99fd245fdaebbcaeb8
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-26 13:15:03 +0200

    Validate userdata input from register and login pages

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,34 @@
-const User = require('../models/User');
+const bcrypt = require("bcrypt");
 
-exports.register = (userData) => User.create(userData);
+const User = require("../models/User");
+
+exports.register = (userData) => {
+    if (userData.password !== userData.rePassword) {
+        throw new Error('Password missmatch!');
+    }
+
+    const user = User.find({email: userData.email});
+
+    if(user) {
+        throw new Error('User exists!');
+    }
+
+    return User.create(userData);
+};
+
+exports.login = async ({ email, password }) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error("Invalid email or password");
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error("Invalid email or password");
+    }
+
+    // Generate token
+
+    // return token
+};
