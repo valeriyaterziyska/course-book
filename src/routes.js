fe2c92891719c170038a7157ff4a2d55203b48aa
Valeria Terziyska <valeria.terziyska@gmail.com>
2025-03-26 12:32:37 +0200

    Add home controller

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-router.get("/", (req, res) => {
-    res.render('home');
-});
+const homeController = require('./controllers/homeController');
+
+router.use(homeController);
 
 
 module.exports = router;
